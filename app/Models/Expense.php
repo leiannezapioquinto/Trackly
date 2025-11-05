e150c9ef50fb59d7b751f37d5bca340ad57cab61
leiannezapioquinto <lei.pioquinto@gmail.com>
2025-11-05 22:21:23 +0800

    = Setup base laravel

---
+++ app/Models/Expense.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'amount', 'date', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
